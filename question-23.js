bd7c10337d52ea461671f6a998e7f7f32aa7694f
hafizabdullahyasir <your-actual-github-email@example.com>
2026-03-07 21:12:01 +0500

    Two Sum (LeetCode #1) 🏆

---
+++ question-23.js
@@ -0,0 +1,17 @@
+// Two Sum (LeetCode #1) 🏆
+
+function twoSum(arr, target) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+
+            // Agar dono ka total target ke barabar hai
+            if (arr[i] + arr[j] === target) {
+                return [arr[i], arr[j]]; // Jawab mil gaya!
+            }
+        }
+    }
+    return "Koi match nahi mila!";
+}
+
+console.log(twoSum([2, 7, 11, 15], 9)); // Output: [2, 7] ✅
+console.log(twoSum([3, 2, 4], 6));      // Output: [2, 4] ✅
